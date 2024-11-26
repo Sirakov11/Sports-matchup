bb4d8bf74b932a589aebe1f215fe3aaa203bf5d3
Sirakov11 <ivansirakov1616@gmail.com>
2024-11-26 11:24:27 +0200

    with issues

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -4,9 +4,11 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
     connection: {
-      database: "sports-matchup",
+      database: "sports_matchup",
       user: "postgres",
-      password: "postgres"
+      password: process.env.DB_PASSWORD,
+      host: "127.0.0.1", 
+      port: 5432   
     },
     pool: {
       min: 2,
@@ -14,6 +16,12 @@ const config: { [key: string]: Knex.Config } = {
     },
     migrations: {
       tableName: "knex_migrations"
+    },
+    seeds: {
+      directory: './seeds'           
     }
+    
   }
 };
+
+module.exports = {config}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,71 @@
 import express, { Express, Request, Response } from 'express';
+import * as bcrypt from 'bcrypt'
+import jwt, { Secret } from 'jsonwebtoken'
+import { User } from './Models/User';
 
+
+//import {config} from './knexfile';
+//import { dbConnection } from './databaseConnection';
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
+app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+const config = require('./knexfile')
+const users: User[] = [];
+const knex = require('knex')(config);
+async function createRecord(tableName:string, data: unknown) {
+  try {
+    const [id] = await knex(tableName).insert(data);
+    return id;
+  } catch (error) {
+    console.error(`Error inserting into ${tableName}:`, error);
+    throw error;
+  }
+}
+
+app.post('/users', async (req, res) => {
+  try {
+    console.log('body', req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+
+    const user:User = {
+      username: req.body.name, password: hashedPassword,
+      email: ''
+    };
+    users.push(user); 
+    console.log(users);
+    res.status(201).send("User created successfully!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error creating user");
+  }
+});
+
+app.post('/login', async (req: Request, res: Response) => {
+  try {
+    const { name, password, email }: { name: string; password: string; email: string } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user: User = {
+      username: name,
+      password: hashedPassword,
+      email: email,
+    };
+
+    users.push(user);
+    console.log(users);
+
+    res.status(201).send("User created successfully!");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error creating user");
+  }
 });
